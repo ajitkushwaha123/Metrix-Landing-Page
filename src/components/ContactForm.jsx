6c2e8b535fc5e3de60531f60b5cd6da7e081cb28
ajitkushwaha123 <ajitkushwahaswe@gmail.com>
2024-08-21 00:37:02 +0530

    contact form backend

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
 import Title from "./Title";
 import Button from "./Button";
+import toast , {Toaster} from "react-hot-toast";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
-  const [country, setCountry] = useState("");
-  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [restourantName, setRestourantName] = useState("");
+
+  const API_URL = "http://localhost:8000/api";
+
+  const formSubmit = async (values) => {
+    console.log(values);
+
+    try{
+      const res = await axios.post(`${API_URL}/contact` , values);
+
+      console.log(res);
+      toast.success("Form Submitted Successfully... !");
+    }
+    catch(err){
+      toast.error(`Error Submitting Form... ! ${err}`);
+    }
+  }
 
   const submitHandler = async (e) => {
     e.preventDefault(); 
@@ -16,16 +34,23 @@ const ContactForm = () => {
       name,
       phone,
       email,
-      country,
-      state,
+      city,
+      restourantName,
     };
+
+    // console.log(user);
+
+    await formSubmit(user);
   };
+
+
 
   return (
     <div
       id="contact-us"
       className="w-full flex flex-col py-[50px] md:py-[100px] justify-center items-center px-[15px] sm:px-[100px] bg-pattern2 "
     >
+      <Toaster reverseOrder="false" position="top-center"></Toaster>
       <Title preTitle={"Book"} title={"Free Demo"} />
       <form
         onSubmit={submitHandler}
@@ -65,7 +90,7 @@ const ContactForm = () => {
           <div className="col-span-2">
             <label className="font-medium">City</label>
             <input
-              onChange={(e) => setCountry(e.target.value)}
+              onChange={(e) => setCity(e.target.value)}
               type="text"
               required
               className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
@@ -75,7 +100,7 @@ const ContactForm = () => {
           <div className="col-span-2">
             <label className="font-medium">Restourant Name</label>
             <input
-              onChange={(e) => setState(e.target.value)}
+              onChange={(e) => setRestourantName(e.target.value)}
               type="text"
               required
               className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
